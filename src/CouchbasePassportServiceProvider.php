a4fc478baf6d181d2312291eb5408b7ff5b908d2
Emmanuel Oduro <emmarthurson@gmail.com>
2018-08-31 16:25:01 +0000

    SMartins\PassportMultiauth Support

--- src/CouchbasePassportServiceProvider.php
+++ src/CouchbasePassportServiceProvider.php
@@ -9,7 +9,7 @@ use Pedmindset\Couchbase\Passport\PersonalAccessClient;
 use Pedmindset\Couchbase\Passport\Token;
 use Pedmindset\Couchbase\Passport\TokenRepository;
 use Pedmindset\Couchbase\Passport\RefreshTokenRepository;
-
+use Pedmindset\Couchbase\Passport\Provider;
 
 
 class CouchbasePassportServiceProvider extends ServiceProvider
@@ -19,24 +19,26 @@ class CouchbasePassportServiceProvider extends ServiceProvider
         /*
          * Passport client extends Eloquent model by default, so we alias them.
          */
-        if (class_exists('Illuminate\Foundation\AliasLoader')) {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-            $loader->alias('Laravel\Passport\AuthCode', AuthCode::class);
-            $loader->alias('Laravel\Passport\Client', Client::class);
-            $loader->alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
-            $loader->alias('Laravel\Passport\Token', Token::class);
-            $loader->alias('Laravel\Passport\TokenRepository', TokenRepository::class);
-            $loader->alias('Laravel\Passport\Bridge\RefreshTokenRepository', RefreshTokenRepository::class);
+            if (class_exists('Illuminate\Foundation\AliasLoader')) {
+                $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+                $loader->alias('Laravel\Passport\AuthCode', AuthCode::class);
+                $loader->alias('Laravel\Passport\Client', Client::class);
+                $loader->alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
+                $loader->alias('Laravel\Passport\Token', Token::class);
+                $loader->alias('Laravel\Passport\TokenRepository', TokenRepository::class);
+                $loader->alias('Laravel\Passport\Bridge\RefreshTokenRepository', RefreshTokenRepository::class);
+                $loader->alias('Laravel\Passport\Bridge\RefreshTokenRepository', RefreshTokenRepository::class);
+                $loader->alias('SMartins\PassportMultiauth\Provider', Provider::class);
 
-        } else {
-            class_alias('Laravel\Passport\AuthCode', AuthCode::class);
-            class_alias('Laravel\Passport\Client', Client::class);
-            class_alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
-            class_alias('Laravel\Passport\Token', Token::class);
-            class_alias('Laravel\Passport\TokenRepository', TokenRepository::class);
-            class_alias('Laravel\Passport\Bridge\RefreshTokenRepository', RefreshTokenRepository::class);
-
-
-        }
+    
+            } else {
+                class_alias('Laravel\Passport\AuthCode', AuthCode::class);
+                class_alias('Laravel\Passport\Client', Client::class);
+                class_alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
+                class_alias('Laravel\Passport\Token', Token::class);
+                class_alias('Laravel\Passport\TokenRepository', TokenRepository::class);
+                class_alias('Laravel\Passport\Bridge\RefreshTokenRepository', RefreshTokenRepository::class);
+                class_alias('SMartins\PassportMultiauth\Provider', Provider::class);
+            }
     }
 }
